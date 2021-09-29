7fac69d2164609c21bb7282bd769a9c967b3d9af
“qa-alam” <alam.itprofessional@gmail.com>
2021-09-29 08:44:58 -0400

    hy

--- src/test/java/com/usa/java/practice/code/CoddingTest.java
+++ src/test/java/com/usa/java/practice/code/CoddingTest.java
@@ -6,6 +6,9 @@ public class CoddingTest {
 	
 	private Color[] colorArray = new Color[6];
 	private int currentIndex = 0;
+	
+	//private int currentIndex = 0;
+
 
 	public void addColorToArray(int red, int green, int blue) {
 	  colorArray[currentIndex++] = new Color(red, green, blue);
